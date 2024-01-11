332d40a8780d2da70bcd330328d16ae1d5880d41
harshit0571 <harshitchadha65@gmail.com>
2024-01-11 11:11:28 +0530

    causes

--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
-const FeaturedCard = ({ raised, goal, title }) => {
+const FeaturedCard = ({ raised, goal, title, img }) => {
+  const perc = goal / raised;
+  const remPerc = 100 - perc;
+  const line1 = "w-[" + perc + "%]";
+  const line2 = "w-[" + remPerc + "%]";
+  console.log(line1 + " " + line2);
   return (
     <div class="featured-card">
       <div class="feature-img">
-        <img src="assets/feature1.png" />
+        <img src={img} />
         <div class="feature-bar">
           <div class="line">
-            <span class="line-1"></span>
-            <span class="line-2"></span>
-            <p>{raised / goal}%</p>
+            <span class={line1 + " line-1"}></span>
+            <span class={line2 + " line-2 "}></span>
+            <p>{goal / raised}%</p>
           </div>
         </div>
       </div>
--- src/components/FeaturedCauses.jsx
+++ src/components/FeaturedCauses.jsx
@@ -1,11 +1,14 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import FeaturedCard from "./FeaturedCard";
 
 const FeaturedCauses = () => {
+  const [data, setdata] = useState([]);
   useEffect(() => {
     const getCauses = async () => {
       const res = await axios.get("http://localhost:8000/causes");
       console.log(res.data);
+      setdata(res.data);
     };
     getCauses();
   }, []);
@@ -16,76 +19,16 @@ const FeaturedCauses = () => {
       </p>
 
       <div class="featured-container">
-        {}
-        <div class="featured-card">
-          <div class="feature-img">
-            <img src="assets/feature1.png" />
-            <div class="feature-bar">
-              <div class="line">
-                <span class="line-1"></span>
-                <span class="line-2"></span>
-                <p>30%</p>
-              </div>
-            </div>
-          </div>
-          <div class="feature-text">
-            <div class="stats">
-              <p>Raised: $300,000</p>
-              <p>Goal: $800,000</p>
-            </div>
-            <div class="feature-heading">
-              Africa Children Need More Food, Water and Clothes
-            </div>
-            <button>Donate Now</button>
-          </div>
-        </div>
-
-        <div class="featured-card">
-          <div class="feature-img">
-            <img src="assets/feature2.png" />
-            <div class="feature-bar">
-              <div class="line">
-                <span class="line-1"></span>
-                <span class="line-2"></span>
-                <p>30%</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="feature-text">
-            <div class="stats">
-              <p>Raised: $300,000</p>
-              <p>Goal: $800,000</p>
-            </div>
-            <div class="feature-heading">
-              Africa Children Need More Food, Water and Clothes
-            </div>
-            <button>Donate Now</button>
-          </div>
-        </div>
-
-        <div class="featured-card">
-          <div class="feature-img">
-            <img src="assets/feature3.png" />
-            <div class="feature-bar">
-              <div class="line">
-                <span class="line-1"></span>
-                <span class="line-2"></span>
-                <p>30%</p>
-              </div>
-            </div>
-          </div>
-          <div class="feature-text">
-            <div class="stats">
-              <p>Raised: $300,000</p>
-              <p>Goal: $800,000</p>
-            </div>
-            <div class="feature-heading">
-              Africa Children Need More Food, Water and Clothes
-            </div>
-            <button>Donate Now</button>
-          </div>
-        </div>
+        {data.map((cause) => {
+          return (
+            <FeaturedCard
+              raised={cause.raised_money}
+              goal={cause.goal}
+              title={cause.title}
+              img={cause.photourl}
+            />
+          );
+        })}
       </div>
     </section>
   );
